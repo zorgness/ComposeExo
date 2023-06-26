9dfca4413716ee586c255aef1b50e5ceaddbd70f
Thomas Fouquert <tfouquert@gmail.com>
2023-06-26 10:07:05 +0200

    add site package

---
+++ app/src/main/java/com/example/efikeyscompose/presentation/site/Site.kt
@@ -0,0 +1,13 @@
+package com.example.efikeyscompose.presentation.site
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SiteScreen() {
+    
+}
+
+@Composable
+fun SiteContent() {
+    
+}
