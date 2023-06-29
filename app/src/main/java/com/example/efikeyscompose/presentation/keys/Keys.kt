f777f0d58b734d798053c253c6da696b15c38698
Thomas Fouquert <tfouquert@gmail.com>
2023-06-29 09:24:27 +0200

    add keys package

---
+++ app/src/main/java/com/example/efikeyscompose/presentation/keys/Keys.kt
@@ -0,0 +1,14 @@
+package com.example.efikeyscompose.presentation.keys
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun KeyScreen() {
+    
+}
+
+@Composable
+fun KeyContent() {
+    
+}
+
---
+++ app/src/main/java/com/example/efikeyscompose/presentation/keys/components/TagButton.kt
@@ -0,0 +1,8 @@
+package com.example.efikeyscompose.presentation.keys.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TagButton() {
+
+}
